f5e977bd0260b032d64271fc98f9a3fde37d6f36
Michael Fedorov <mikhailfedorov1939@gmail.com>
2022-05-21 20:14:23 +0300

    Add the new function

--- prekolk/task_list.py
+++ prekolk/task_list.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 from math import sqrt
+from os import lseek
 
 # Solution for Lesson Three
 def get_square_roots(num):
@@ -28,14 +29,20 @@ def get_range(num):
 def duplicate(list):
     list = list.extend(list)
 
+# Solution for lesson Six
+def rotate(list):
+    if list != []:
+        list.insert(0, list.pop(-1))
 
 def main():
     print(get_range(10))
     print(get_square_roots(49))
     
-    list = ['nn', 'hh']
-    duplicate(list)
-    print(list)
+    lst = [1, 3, 5]
+    duplicate(lst)
+    print(lst)
+    rotate(lst)
+    print(lst)
 
 if __name__ == '__main__':
     main()
