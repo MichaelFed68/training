5807d06b2949510c033673b155a2a04fc0d0d4ff
Michael Fedorov <mikhailfedorov1939@gmail.com>
2022-05-23 20:40:28 +0300

    Add the new functions

--- prekolk/task_list.py
+++ prekolk/task_list.py
@@ -34,15 +34,27 @@ def rotate(list):
     if list != []:
         list.insert(0, list.pop(-1))
 
+# Solution for lesson Seven
+def rotaded_left(obj):
+    return obj[1:] + obj[:1]
+
+def rotaded_right(obj):
+    return obj[-1:]+ obj[:-1]
+
 def main():
     print(get_range(10))
     print(get_square_roots(49))
     
     lst = [1, 3, 5]
+
     duplicate(lst)
     print(lst)
+
     rotate(lst)
     print(lst)
+
+    print(rotaded_right(lst))
+    print(rotaded_left(lst))
 
 if __name__ == '__main__':
     main()
