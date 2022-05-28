05b888e3e3f498f44887c381c1261c74d63d1b1d
Michael Fedorov <mikhailfedorov1939@gmail.com>
2022-05-28 15:22:05 +0300

    Add the new function and refactoring

--- prekolk/task_list.py
+++ prekolk/task_list.py
@@ -23,15 +23,21 @@ def get_range(num):
             res.append(i)
             i += 1
         return res
+# END
+
 
 # Solution for Lesson Five
 def duplicate(list):
     list = list.extend(list)
+# END
+
 
 # Solution for lesson Six
 def rotate(list):
     if list != []:
         list.insert(0, list.pop(-1))
+# END
+
 
 # Solution for lesson Seven
 def rotaded_left(obj):
@@ -39,23 +45,34 @@ def rotaded_left(obj):
 
 def rotaded_right(obj):
     return obj[-1:]+ obj[:-1]
+# END
+
 
 # Solution for lesson Eigth
-def find_index(value, data):
-    for (index, element) in enumerate(data):
-        if element == value:
+def find_index(value, list):
+    for (index, item) in enumerate(list):
+        if item == value:
             return index 
     else:
         return None
+# END
+
+
+# Solution for lesson Nine
+def find_second_index1(value, list):
+    iterator = iter(list)
+    first_index = find_index(value, iterator)
+    if first_index != None:
+        second_index = find_index(value, iterator)
+        if second_index != None:
+            return first_index + second_index + 1
+    return None
+#END
 
 
 def main():
-    print(get_range(10))
-    print(get_square_roots(49))
-    
-    lst = 'ddiirir4'
-
-    print(find_index('4', lst))
+    lis = [2929, 'FFF,', 19, 10]
+    print(find_second_index1(19, lis))
 
 if __name__ == '__main__':
     main()
