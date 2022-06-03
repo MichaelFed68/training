7cffef67ff0af1855254bef3cd6822bd732d2f3b
Michael Fedorov <mikhailfedorov1939@gmail.com>
2022-06-03 13:09:20 +0300

    Add solution lesson three to task_dict.py

--- prekolk/task_dict.py
+++ prekolk/task_dict.py
@@ -6,4 +6,16 @@ def make_user (name, age):
     
 def format_user(user):
     return f"{user.get('name')}, {user.get('age')}"
-#END
+# END
+
+
+# Soolution for lesson Three
+def count_all(date):
+    dict = {}
+    for i in date:
+        if i in dict:
+            continue
+        count_keys = date.count(i)
+        dict[i] = count_keys
+    return dict
+# END
