90df0ca05da750c256d5b130e819b9e91d779bf4
Michael Fedorov <mikhailfedorov1939@gmail.com>
2022-06-10 18:11:22 +0300

    Add solution lesson Three

--- prekolk/function/task_functon.py
+++ prekolk/function/task_functon.py
@@ -2,14 +2,27 @@
 
 
 # Solution Two
+from ctypes.wintypes import RGB
+
+
 def greet(name, *args):
     names = " and ".join((name,) + args)
     return 'Hello, {}!'.format(names)
 
 # END
 
-# Solution Three
 
+# Solution Three
+def rgb(red=0, green=0, blue=0):
+    return 'rgb({}, {}, {})'.format(red, green, blue)
+
+def get_colors():
+
+    return dict(
+        red=rgb(red=255),
+        green=rgb(green=255),
+        blue=rgb(blue=255)
+        )
 # END
 
 
@@ -22,23 +35,9 @@ def greet(name, *args):
 
 # END
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 def main():
-    print(greet('1', 'fg'))
+    colors = get_colors()
+    print(colors['green'])
 
 if __name__ == '__main__':
     main()
