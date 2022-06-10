5a98265c4cfaf618a9473598a83db2fc438f6654
Michael Fedorov <mikhailfedorov1939@gmail.com>
2022-06-10 17:27:16 +0300

    Add solution lesson Two

---
+++ prekolk/function/task_functon.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+
+# Solution Two
+def greet(name, *args):
+    names = " and ".join((name,) + args)
+    return 'Hello, {}!'.format(names)
+
+# END
+
+# Solution Three
+
+# END
+
+
+# Solution Four
+
+# END
+
+
+# Solution Five
+
+# END
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def main():
+    print(greet('1', 'fg'))
+
+if __name__ == '__main__':
+    main()
