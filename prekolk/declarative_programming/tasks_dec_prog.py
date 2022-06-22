6790c8e106c2373e86bece78e256cb3e817178d5
Michael Fedorov <mikhailfedorov1939@gmail.com>
2022-06-22 16:11:23 +0300

    Create package declarative_programming

---
+++ prekolk/declarative_programming/tasks_dec_prog.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+
+# Solution lesson Two
+
+# END
+
+
+# Solution lesson Three
+
+# END
+
+
+# Solution lesson Four
+
+# END
+
+
+# Solution lesson Five
+
+# END
+
+
+# Solution lesson Six
+
+# END
+
+
+def main():
+    l = 5
+
+
+if __name__ == '__main__':
+    main()
