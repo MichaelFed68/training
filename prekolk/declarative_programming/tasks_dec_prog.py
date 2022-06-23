48ff546476e772ec712ea523d11711cea880393e
Michael Fedorov <mikhailfedorov1939@gmail.com>
2022-06-23 22:36:24 +0300

    Add solution number Two, Three

--- prekolk/declarative_programming/tasks_dec_prog.py
+++ prekolk/declarative_programming/tasks_dec_prog.py
@@ -2,12 +2,28 @@
 
 
 # Solution lesson Two
+from operator import itemgetter
+def get_odds(x):
+    itemgetter(slice(None, None, 2))
 
+def get_odds_2(x):
+    return x[::2]
+    
+def odds_from_odds(list_of_lists):
+    return list(map(get_odds, get_odds(list_of_lists)))
+
+
+
+def keep_odds_from_odds(list_of_lists):
+    del list_of_lists[1::2]
+    for item in list_of_lists:
+       del item[1::2]
 # END
 
 
 # Solution lesson Three
-
+def non_empty_truths(list_of_lists):
+   return [list_ for list_ in [[item for item in list_ if item] for list_ in list_of_lists] if list_]
 # END
 
 
