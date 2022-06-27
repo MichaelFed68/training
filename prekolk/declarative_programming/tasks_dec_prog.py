423b63f7d0ed7f96b7bfd55ca0f06bcaa91ea5cd
Michael Fedorov <mikhailfedorov1939@gmail.com>
2022-06-27 18:46:14 +0300

    Add solution lesson Five

--- prekolk/declarative_programming/tasks_dec_prog.py
+++ prekolk/declarative_programming/tasks_dec_prog.py
@@ -19,13 +19,19 @@ def odds_from_odds(list_of_lists):
 def keep_odds_from_odds(list_of_lists):
     del list_of_lists[1::2]
     for item in list_of_lists:
-       del item[1::2]
+        del item[1::2]
 # END
 
 
 # Solution lesson Three
 def non_empty_truths(list_of_lists):
-   return [list_ for list_ in [[item for item in list_ if item] for list_ in list_of_lists] if list_]
+    return [
+        truth for truth in [
+            [item for item in list_ if item]
+            for list_ in list_of_lists
+        ]
+        if truth
+    ]
 # END
 
 
@@ -36,7 +42,29 @@ def number_of_unique_letters(str_):
 
 
 # Solution lesson Five
+def is_int(x):
+    return isinstance(x, int)
 
+
+def each2d(test, matrix):
+    return all(
+        all(test(item) for item in row)
+        for row in matrix
+    )
+
+
+def somy2d(test, matrix):
+    return any(
+        any(test(item) for item in row)
+        for row in matrix
+    )
+
+
+def sum2d(test, matrix):
+    return sum(
+        sum(item for item in row if test(item))
+        for row in matrix
+    )
 # END
 
 
@@ -46,7 +74,7 @@ def number_of_unique_letters(str_):
 
 
 def main():
-    l = 5
+    print('q')
 
 
 if __name__ == '__main__':
