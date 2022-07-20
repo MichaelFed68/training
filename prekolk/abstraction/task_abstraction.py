f3139a3a2f935d9db34e36d26eaa2ed2f895b0eb
Michael Fedorov <mikhailfedorov1939@gmail.com>
2022-07-20 13:30:47 +0300

    Add solution lesson Four to task_abstraction.py

--- prekolk/abstraction/task_abstraction.py
+++ prekolk/abstraction/task_abstraction.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-
 from math import sqrt
 
 
@@ -13,6 +12,34 @@ def calculate_distance(point1, point2):
 
 
 # Solution Four
+def make_decart_point(x, y):
+    return {'x': x, 'y': y}
+
+
+def get_x(point):
+    return point['x']
+
+
+def get_y(point):
+    return point['y']
+
+
+def make_segment(point1, point2):
+    return {'begin_point': point1, 'end_point': point2}
+
+
+def get_mid_point_of_segment(segment):
+    x = (segment['begin_point']['x'] + segment['end_point']['x']) / 2
+    y = (segment['begin_point']['y'] + segment['end_point']['y']) / 2
+    return {'x': x, 'y': y}
+
+def test_get_mid_point_of_segment():
+    point1 = make_decart_point(3, 2)
+    point2 = make_decart_point(0, 0)
+    segment = make_segment(point1, point2)
+    assert get_mid_point_of_segment(segment) == {'x': 1.5, 'y': 1}
+
+test_get_mid_point_of_segment()
 # END
 
 
--- tests/abstraction/test_task_abstraction.py
+++ tests/abstraction/test_task_abstraction.py
@@ -1,9 +1,13 @@
+from turtle import right
 import pytest
 from prekolk.abstraction.task_abstraction import calculate_distance
+from prekolk.abstraction.task_abstraction import make_decart_point
+from prekolk.abstraction.task_abstraction import get_x, get_y
+from prekolk.abstraction.task_abstraction import make_segment
+from prekolk.abstraction.task_abstraction import get_mid_point_of_segment
 
 
 def test_calculate_distance():
-    assert calculate_distance([0, 0], [3, 4]) == 5.0
     assert calculate_distance([5, 2], [7, 8]) == 6.324555320336759
     assert calculate_distance([-5, 2], [-7, -8]) == 10.198039027185569
 
@@ -11,3 +15,27 @@ def test_calculate_distance():
 def test_with_empty_lists():
     with pytest.raises(ValueError):
         calculate_distance([], [])
+
+
+def test_make_decart_point():
+    assert make_decart_point(1, 4) == {'x': 1, 'y': 4}
+
+
+def test_selectors_get_x_and_get_y():
+    point = make_decart_point(1, 4)
+    assert get_x(point) == 1
+    assert get_y(point) == 4
+
+
+def test_make_segment():
+    point1 = make_decart_point(3, 2)
+    point2 = make_decart_point(0, 0)
+    dictionary = {'begin_point': point1, 'end_point': point2}
+    assert make_segment(point1, point2) == dictionary
+
+
+def test_get_mid_point_of_segment():
+    point1 = make_decart_point(3, 2)
+    point2 = make_decart_point(1, -4)
+    segment = make_segment(point1, point2)
+    assert get_mid_point_of_segment(segment) == {'x': 2.0, 'y': -1.0}
