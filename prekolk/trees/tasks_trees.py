c7e47e633aee0d3e12a23f0e4e0b4e0af9d94e16
Michael Fedorov <mikhailfedorov1939@gmail.com>
2022-07-23 13:11:36 +0300

    Create a package trees

---
+++ prekolk/trees/tasks_trees.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+
+# Lesson Two
+# END
+
+
+# Lesson Three
+# END
+
+
+# Lesson Four
+# END
+
+
+# Lesson Five
+# END
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
