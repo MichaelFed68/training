a700d712b70c27f2cb53c6343e9e2b8822c048a9
Michael Fedorov <mikhailfedorov1939@gmail.com>
2022-08-23 12:03:58 +0300

    Add solution lesson Three to the task_oop.py

---
+++ prekolk/oop/task_oop.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+
+# Solution Three
+def rgb(red, green, blue):
+    return red, green, blue
+
+
+class Color:
+    red = rgb(255, 0, 0)
+    green = rgb(0, 255, 0)
+    blue = rgb(0, 0, 255)
+# END
+
+
+# Solution Four
+
+# END
+
+
+# Solution Three
+# END
+
+# Solution Three
+# END
+
+# Solution Three
+# END
+
+# Solution Three
+# END
+
+# Solution Three
+# END
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
